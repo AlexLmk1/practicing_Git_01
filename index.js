e75c947e5578b12f10bc8dfe29d0fb0aa0a6cb48
AlexLmk1 <a.s.lozanomorales@gmail.com>
2026-03-05 13:43:14 +0100

    mathematical idea

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("******************************");
 console.log("hi world _ testing ");
 console.log("******************************");
+console.log(" 2 +2 =1 depending in which base is the sum, right? ");
